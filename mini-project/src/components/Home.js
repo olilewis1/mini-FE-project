687e5f1eee0d5105223441afe5b836c70fdede12
Oli Lewis <olewis@UK06346.local>
2021-11-30 10:49:54 +0000

    added search and mapped to page

--- mini-project/src/components/Home.js
+++ mini-project/src/components/Home.js
@@ -1,18 +1,44 @@
 import React, { useState } from 'react'
 import RandomGif from './RandomGif'
-
+import axios from 'axios'
 const Home = () => {
-
+    // handle random gif
     const [trueOrFalseForGif, setTrueOrFalseForGif] = useState(undefined)
 
     const handleRandomGif = async () => {
-        if (trueOrFalseForGif === 2 ) {
+        if (trueOrFalseForGif === 2) {
             setTrueOrFalseForGif(1)
         }
         if (trueOrFalseForGif === 1 || trueOrFalseForGif === undefined) {
             setTrueOrFalseForGif(2)
         }
     }
+    // end of handle random gid 
+
+    const [searchTerm, setSearchTerm] = useState('')
+    const [searchGifs, setSearchGifs] = useState(false)
+    const handleChange = (event) => {
+        const searchVariable = event.target.value
+        setSearchTerm(searchVariable)
+    }
+
+    const handleSubmitSearch = (event) => {
+        console.log(event)
+        console.log(searchTerm)
+        const getData = async () => {
+            try {
+                const { data } = await axios.get(`http://api.giphy.com/v1/gifs/search?api_key=P8JU8Vdoef6GiFlYQpEc7U9OVqf8dvjb&q=${searchTerm}&limit=20`)
+                const response = data.data
+                console.log('response', response)
+                setSearchGifs(response)
+            } catch (err) {
+                console.log('error', err)
+            }
+        }
+        getData()
+    }
+
+
 
     return (
         <>
@@ -27,13 +53,21 @@ const Home = () => {
                             id="gif-search"
                             placeholder="Search gifs"
                             name="s"
+                            onChange={handleChange}
+                            value={searchTerm}
                         />
+                        <button type="submit" onClick={handleSubmitSearch}>Search</button>
                     </div>
                 </div >
             </div>
             {trueOrFalseForGif ? <RandomGif {...trueOrFalseForGif} /> : <div>
                 Pick a random div</div>}
-            
+            {searchGifs ? searchGifs.map(item =>
+                <div key={item.id}>
+                    <img src={item.images.downsized_large.url} alt="gif" />
+                </div>
+            ) : <div> make a search </div>
+            }
 
         </>
     )
